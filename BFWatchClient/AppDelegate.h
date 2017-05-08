6ce1c4924baa39483da93a9b088ed57c20eb2dd5
BFAlex <838318741@qq.com>
2017-05-08 19:51:11 +0800

    Initial Commit

---
+++ BFWatchClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFWatchClient
+//
+//  Created by Readboy_BFAlex on 2017/5/8.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFWatchClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFWatchClient
+//
+//  Created by Readboy_BFAlex on 2017/5/8.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
